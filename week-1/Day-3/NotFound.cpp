371110ad41a00e4813470536ff2447d69e2acfdc
Sharna Rani Paul <130475193+Sharna2002@users.noreply.github.com>
2024-03-18 23:52:18 +0600

    Update NotFound.cpp

--- week-1/Day-3/NotFound.cpp
+++ week-1/Day-3/NotFound.cpp
@@ -1,30 +1,40 @@
-#include<bits/stdc++.h>
-using namespace std;
+#include <bits/stdc++.h>
 #define sp   <<" "
 #define nl   <<"\n"
 #define yes  cout << "YES"
 #define no   cout << "NO"
 #define int  long long int
-
+using namespace std;
 
 int32_t main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-  
+    
     string s;
     cin >> s;
-    sort(s.begin(),s.end());
-    cout << s;
-    // vector<int>v(26);
-    // for(int i=0;i<s.size();i++)
-    // {
-    //    int val = s[i] - 'a';
-    //    v[val]++;
-    // }
-    //  for(int i=0;i<s.size();i++)
-    // {
-    //    char val = s[i] - 'a';
-    //    cout << val + 'a' <<" : " << v[val]<<"\n";
-    //}
-	 return 0;
+    //sort(s.begin(),s.end());
+    vector<int>v(26);
+    for(int i=0;i<s.size();i++)
+    {
+       int val = s[i] - 97;
+        v[val]++;
+    }
+    char ans ;
+    int f = 0;
+    for(int i=0;i<26;i++)
+    {
+        if(v[i] == 0)
+        {
+            char c = i + 'a';
+            ans = c;
+            f = 1;
+            break;
+         }
+
+    }
+    if(f)
+        cout << ans nl;
+    else
+        cout <<"None" nl;
+    return 0;
 }
